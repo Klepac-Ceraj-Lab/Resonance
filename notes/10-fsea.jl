e96a26e941ccfe8270a1ac97d897e64557aa0acd
Kevin Bonham, PhD <kbonham@wellesley.edu>
2022-07-20 16:55:09 -0400

    complete brain fsea... for now

--- notes/10-fsea.jl
+++ notes/10-fsea.jl
@@ -154,49 +154,67 @@ for region in brain_features
         map(collect(keys(neuroactive))) do gs
             @info gs
             ixs = neuroactive[gs]
-            isempty(ixs) && return (; region, geneset = gs, U = NaN, median = NaN, mu = NaN, sigma = NaN, pvalue = NaN, cs = Float64[], acs = Float64[])
+            isempty(ixs) && return (; region, geneset = gs, U = NaN, median = NaN, mu = NaN, sigma = NaN, pvalue = NaN)
 
             cs = filter(!isnan, cscor[ixs])
-            isempty(cs) && return (; region, geneset = gs, U = NaN, median = NaN, mu = NaN, sigma = NaN, pvalue = NaN, cs = Float64[], acs = Float64[])
+            isempty(cs) && return (; region, geneset = gs, U = NaN, median = NaN, mu = NaN, sigma = NaN, pvalue = NaN)
 
             acs = filter(!isnan, cscor[Not(ixs)])
             mwu = MannWhitneyUTest(cs, acs)
 
-            return (; region, geneset = gs, U = mwu.U, median = mwu.median, mu = mwu.mu, sigma = mwu.sigma, pvalue=pvalue(mwu), cs, acs)
+            return (; region, geneset = gs, U = mwu.U, median = mwu.median, mu = mwu.mu, sigma = mwu.sigma, pvalue=pvalue(mwu))
         end
     )
     subset!(fsdf, :pvalue=> ByRow(!isnan))
     append!(brfsea, fsdf)
 end
 
+
 brfsea.qvalue = adjust(brfsea.pvalue, BenjaminiHochberg())
 sort!(brfsea, :qvalue)
-CSV.write(datafiles("fsea_brain_all.csv"), select(brfsea, Not(["cs", "acs"])))
+CSV.write(datafiles("fsea_brain_all.csv"), brfsea)
 
-pretty_table(first(select(brfsea, Not(["cs", "acs"])), 10); backend = Val(:latex))
+pretty_table(first(brfsea, 10); backend = Val(:latex))
 
 #-
 
 using Connectomes
-using CairoMakie
+using WGLMakie
 using ColorSchemes
+WGLMakie.activate!()
 
 c = Connectomes.connectomepath() |> Connectome
+brfsea = CSV.read(datafiles("fsea_brain_all.csv"), DataFrame)
+brgroup = groupby(brfsea, :geneset)
 
 #-
 
+gr = first(brgroup)
+gr = subset(gr, :pvalue => ByRow(<(0.05)))
+
+
+
+#-
 fig = Figure(resolution=(1200,600))
 ax = Axis3(fig[1,1], aspect = :data, azimuth = 0.0pi, elevation=0.0pi)
 hidedecorations!(ax)
 hidespines!(ax)
 
+plotted = Int[]
 
-for i in 1:83
-    plot_roi!(i, get(ColorSchemes.viridis, rand()))
+for (r, m) in zip(gr.region, gr.mu)
+    i = findfirst(l-> contains(l, replace(r, "_"=>"-")), c.parc.Label)
+    i = isnothing(i) && r == "Brain_Stem" ? 83 : i
+    isnothing(i) && continue
+    push!(plotted, i)
+    plot_roi!(i, get(ColorSchemes.viridis, m / 3e7))
 end
- 
-fig
 
+for i in setdiff(c.parc.ID, plotted)
+    plot_roi!(i, (:gray, 0.2))
+end
+
+fig
 
 #- 
 
