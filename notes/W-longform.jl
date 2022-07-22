b048967b03a1ee12f3f57024a4b1dd6948e398b7
Guilherme Fahur Bottino <gfahurbottino@gmail.com>
2022-07-22 15:54:46 -0400

    Added a perform_airtable_write bool to toggle the write portion of the code

--- notes/W-longform.jl
+++ notes/W-longform.jl
@@ -1,11 +1,19 @@
-include("startup.jl")
+using Resonance, CategoricalArrays
+
+#include(joinpath(pathof(Resonance), "..", "..", "notes", "01-metadata_wrangle.jl"))
+#include(joinpath(pathof(Resonance), "..", "..", "notes", "02-species.jl"))
+#include(joinpath(pathof(Resonance), "..", "..", "notes", "03-metabolites.jl"))
+#include(joinpath(pathof(Resonance), "..", "..", "notes", "04-functions.jl"))
+include(joinpath(pathof(Resonance), "..", "..", "src", "startup.jl"))
+
+omni, etoh, tps, complete_brain, metabolites, species = startup()
 
 for col in names(tps)
     tps[!, col] = collect(tps[!, col])
 end
 
-tps_specific = stack(tps, ["ageMonths", "cogScore", "ECHOTPCoded", brainmeta...], [:subject, :timepoint])
-subj_specific = stack(tps, [:mother_HHS_Education, :simple_race], [:subject])
+tps_specific = stack(tps, ["ageMonths", "cogScore", "ECHOTPCoded", brainmeta_underscore...], [:subject, :timepoint])
+subj_specific = stack(tps, [:mother_HHS_Education, :rce], [:subject]) # changed simple_race to rce
 grp = groupby(subj_specific, [:subject, :variable])
 subj_specific = DataFrames.combine(grp, :value=> (v-> coalesce(v...))=> :value)
 
@@ -27,9 +35,8 @@ for s in samples(species)
     end
 end
 
-CSV.write(datafiles("wrangled", "tidy_timepoints_with_brain.csv", tps_specific)
-CSV.write(datafiles("wrangled", "tidy_subjects.csv", subj_specific)
+CSV.write(datafiles("wrangled", "tidy_timepoints_with_brain.csv"), tps_specific)
+CSV.write(datafiles("wrangled", "tidy_subjects.csv"), subj_specific)
 
 
 ##
-
