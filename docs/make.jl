227dbfae4c2354d39564b54781b5adecdea56556
Kevin Bonham, PhD <kbonham@wellesley.edu>
2022-08-01 22:59:38 -0400

    Documenter build

--- docs/make.jl
+++ docs/make.jl
@@ -4,17 +4,17 @@ using Documenter
 DocMeta.setdocmeta!(Resonance, :DocTestSetup, :(using Resonance); recursive=true)
 
 makedocs(;
-    modules=[Resonance],
-    authors="Kevin Bonham, PhD <kbonham@wellesley.edu> and contributors",
-    repo="https://github.com/Klepac-Ceraj-lab/Resonance/blob/{commit}{path}#{line}",
-    sitename="Resonance",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://Klepac-Ceraj-lab.github.io/Resonance",
-        edit_link="main",
-        assets=String[],
+    modules = [Resonance],
+    authors = "Kevin Bonham, PhD <kbonham@wellesley.edu> and contributors",
+    repo = "https://github.com/Klepac-Ceraj-lab/Resonance/blob/{commit}{path}#{line}",
+    sitename = "Resonance",
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://Klepac-Ceraj-lab.github.io/Resonance",
+        edit_link = "main",
+        assets = String[],
     ),
-    pages=[
+    pages = [
         "Home" => "index.md",
         "Setup" => "setup.md",
         "Figures" => [
@@ -26,6 +26,7 @@ makedocs(;
 )
 
 deploydocs(;
-    repo="github.com/Klepac-Ceraj-lab/Resonance",
-    devbranch="main",
+    repo = "github.com/Klepac-Ceraj-lab/Resonance",
+    push_preview = true,
+    devbranch = "main",
 )
