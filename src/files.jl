728caede7d59f018275b0679f4619a4269b7c306
Kevin Bonham, PhD <kbonham@wellesley.edu>
2022-08-02 10:10:14 -0400

    begin working on tests

--- src/files.jl
+++ src/files.jl
@@ -3,7 +3,9 @@ scratchfiles(args...) = joinpath(get(ENV, "SCRATCH_SPACE", "./scratch"), args...
 datafiles(args...) = joinpath(get(ENV, "DATA_FILES", "./data"), args...)
 
 @testset "Dir functions" begin
-    @test analysisfiles("test", "thing.txt") == "./analysis/test/thing.txt"    
-    @test scratchfiles("test", "thing.txt") == "./data/test/thing.txt"    
-    @test datafiles("test", "thing.txt") == "./data/test/thing.txt"    
+    withenv("ANALYSIS_FILES"=> nothing, "SCRATCH_SPACE"=> nothing, "DATA_FILES" => nothing) do
+        @test analysisfiles("test", "thing.txt") == "./analysis/test/thing.txt"
+        @test scratchfiles("test", "thing.txt") == "./scratch/test/thing.txt"
+        @test datafiles("test", "thing.txt") == "./data/test/thing.txt"
+    end
 end
---
+++ test/runtests.jl
@@ -0,0 +1,5 @@
+include("ResonanceTests.jl")
+ResonanceTests.runtests()
+
+using Resonance
+Resonance.runtests()
