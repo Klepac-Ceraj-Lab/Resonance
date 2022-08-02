ede68b19cc49df40f531394a30d9e1d4e71d62b1
Kevin Bonham, PhD <kbonham@wellesley.edu>
2022-08-02 09:51:16 -0400

    fix case?

--- docs/make.jl
+++ docs/make.jl
@@ -6,7 +6,7 @@ DocMeta.setdocmeta!(Resonance, :DocTestSetup, :(using Resonance); recursive=true
 makedocs(;
     modules = [Resonance],
     authors = "Kevin Bonham, PhD <kbonham@wellesley.edu> and contributors",
-    repo = "https://github.com/Klepac-Ceraj-lab/Resonance/blob/{commit}{path}#{line}",
+    repo = "https://github.com/Klepac-Ceraj-Lab/Resonance/blob/{commit}{path}#{line}",
     sitename = "Resonance",
     format = Documenter.HTML(;
         prettyurls = get(ENV, "CI", "false") == "true",
