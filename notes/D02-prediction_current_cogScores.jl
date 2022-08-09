b276441d8bed93b7e43ea49028ad2e7258b5cce4
Guilherme Fahur Bottino <gfahurbottino@gmail.com>
2022-08-09 01:14:00 -0400

    Added a script for prediction of current cogscore as a function of ageMonths

--- notes/D02-prediction_current_cogScores.jl
+++ notes/D02-prediction_current_cogScores.jl
@@ -1,196 +1,224 @@
-# #Machine Learning Models
+#####
+# Notebook D02 - Prediction of futureCogScores from current taxonomic data
+#####
 
-# ## Loading packages
-
-using Resonance
-using CairoMakie
+using Chain
 using CSV
-using DataFrames
-using MLJ
 using Statistics
+using Random
+using DataFrames
 using StableRNGs
-ml_rng = StableRNG(102528)
+using ProgressMeter
+using MLJ
+ml_rng = StableRNG(0)
 
-# ## Parsing tidy CSV data
+# ## 1. Collecting the data to make the future dataframe
+include("D00-collect_taxonomic_cogscore_data.jl")
 
-rawDf = CSV.read(
-    joinpath("data", "tidy_timepoints_with_brain.csv"),
-    DataFrame;
-    delim = ',',
-    types = [Int64, Int64, String, String],
-    header = ["subject", "timepoint", "variable", "value"],
-    skipto = 2
-)
+max_prediction_ageMonths = 24
 
-# ### Fixing some metadata names (see source for details)
-
-#fix_metadata_colnames!(rawDf)
-
-# ## Removing duplicates
-
-# ### Removing actual duplicated lines
-
-check_longdata_metaduplicates!(rawDf; remove_duplicates=true)
-
-# ### Removing technical/biological replicates
-
-retainfirst = get(ENV, "RETAIN_FIRST", true) # if true, retain the first technical/biological replicate; if false, retain the last technical/biological replicate
-
-if retainfirst
-
-    unstackedDf = @chain rawDf begin
-        groupby( [:subject, :timepoint, :variable] )
-        combine(:value=>first)
-        unstack(:variable, :value_first)
-    end
-
-else 
-    unstackedDf = unstack(rawDf, :variable, :value; allowduplicates=true)
-end
-
-## Prediction workflows
-
-### Problem 1: predict current cogScore from current taxonomic profile
-
-#### Building predictionDf
-
-select_columns = [ "cogScore", "ageMonths", microbiome_predictors... ]
-
-##### 1. Removing rows with missing values
-
-predictionDf = @chain unstackedDf begin
-    select(select_columns)
+### 2.2. Subsetting data
+prediction_df = @chain cogscore_taxa_df begin
     subset(:ageMonths => x -> .!(ismissing.(x)))
     subset(:cogScore => x -> .!(ismissing.(x)))
-    subset(microbiome_predictors[1] => x -> .!(ismissing.(x)))
+    subset(:sample => x -> .!(ismissing.(x)))
+    dropmissing()
+    subset(:ageMonths => x -> x .<= max_prediction_ageMonths)
+    # unique( [:subject] )
+    # DataFrames.select(Not(:timepointDelta))
 end
 
-mapcols!(col -> parse.(Float64, col), predictionDf)
-
-##### 2. Filtering by ageMonths < max_prediction_age
-
-max_prediction_age = 120
-
-predictionDf = @chain predictionDf begin
-    subset(:ageMonths => x ->  x .<= max_prediction_age)
-    DataFrames.transform(:ageMonths => ByRow(x -> x / max_prediction_age) => [:ageMonths])
-end
-
-##### 3. Removing microbiome columns with zero standard deviation
-
-standard_deviations = [ std(predictionDf[:,i]) for i in 1:ncol(predictionDf) ]
-is_zerostd_col = standard_deviations .== 0
-is_negligiblestd_col = standard_deviations .<= 0.1
-zerostd_cols = findall(is_zerostd_col)
-negligiblestd_cols = findall(is_negligiblestd_col)
-zerostd_colnames = names(predictionDf)[zerostd_cols]
-negligiblestd_colnames = names(predictionDf)[negligiblestd_cols]
-
-predictionDf = select(predictionDf, Not(negligiblestd_colnames))
-
-#### Splitting training data between train and test
-
-train, test = partition(eachindex(1:nrow(predictionDf)), 0.7, shuffle=true, rng=ml_rng)
-
-#### Checking the schema and MLJ scitypes
-
-schema(predictionDf)
-
-#### Separating the predictors and target variable, vewing matching machine models
-
-y, X = unpack(predictionDf, ==(:cogScore); rng=ml_rng);
-
+### 2.4. Separating predictors and target from the additional metadata
+y = prediction_df.cogScore
+X = dropmissing(prediction_df[:, names(prediction_df) .∈ Ref(retained_featurenames)])
 for m in models(matching(X,y)) println(m) end
 
-#### Building the MLJ machine for target problem.
+## 3. Training the predictor model
 
+### 3.1. Loading the desired model
 RandomForestRegressor = @load RandomForestRegressor pkg=DecisionTree
-Standardizer = @load Standardizer pkg=MLJModels
 
-input_standardizer = Standardizer()
-input_standardization_machine = machine(input_standardizer, X)
-fit!(input_standardization_machine, rows = train)
-X_scaled = MLJ.transform(input_standardization_machine, X)
+#### Splitting training data between train and test
+Random.seed!(ml_rng, 1)
+train, test = partition(eachindex(1:nrow(X)), 0.6, shuffle=true, rng=ml_rng)
 
-target_standardizer = Standardizer()
-target_standardization_machine = machine(target_standardizer, y)
-fit!(target_standardization_machine, rows = train)
-y_scaled = MLJ.transform(target_standardization_machine, y)
+### 2.5. Removing microbiome columns with zero standard deviation
+standard_deviations = [ std(X[train,i]) for i in 1:ncol(X) ]
+is_zerostd_col = standard_deviations .== 0
+println("$(sum(is_zerostd_col)) columns have zero standard deviation!")
+zerostd_cols = findall(is_zerostd_col)
+zerostd_colnames = names(X)[zerostd_cols]
+X = DataFrames.select(X, Not(zerostd_colnames))
 
-rf_model = RandomForestRegressor(n_subfeatures = 0)
+    # Declaring the optimization grid
+    nodesize_range = collect(1:2:25)
+    maxnodes_range = collect(1:2:25)
+    sampsize_range = collect(10:20:110) ./ nrow(X[train, :])
+    mtry_range = collect(5:2:30)
+    n_trees_range = [ 500 ]
 
-# Hyperparameters of DecisionTree.RandomForestRegressor:
-# ```
-# max_depth = -1, 
-# min_samples_leaf = 1, 
-# min_samples_split = 2, 
-# min_purity_increase = 0.0, 
-# n_subfeatures = -1, 
-# n_trees = 10, 
-# sampling_fraction = 0.7
-# ````
+    tuning_grid = vec(collect(Base.product(
+        nodesize_range,
+        maxnodes_range,
+        sampsize_range,
+        mtry_range,
+        n_trees_range
+    )))
 
-#range_max_depth = range(rf_model, :max_depth; lower=-1, upper=5)
-range_samples_leaf = range(rf_model, :min_samples_leaf; lower=1, upper=5)
-range_min_samples_split = range(rf_model, :min_samples_split; lower=1, upper=5)
-range_min_purity_increase = range(rf_model, :min_purity_increase; lower=0.0, upper=0.3)
-range_n_trees = range(rf_model, :n_trees; lower=50, upper=450)
-range_sampling_fraction = range(rf_model, :sampling_fraction; lower=0.5, upper=0.9)
+train_mae_vector = Vector{Float64}(undef, length(tuning_grid))
+train_cor_vector = Vector{Float64}(undef, length(tuning_grid))
+test_mae_vector = Vector{Float64}(undef, length(tuning_grid))
+test_cor_vector = Vector{Float64}(undef, length(tuning_grid))
 
-ranges_vector = [
-#    range_max_depth,
-    range_samples_leaf,
-#    range_min_samples_split,
-#    range_min_purity_increase,
-    range_n_trees,
-#    range_sampling_fraction
-]
+bestcor = 0.0
 
-tunable_rf_model = TunedModel(
-    model = rf_model,
-    ranges = ranges_vector,
-    resampling = CV(nfolds=3),
-    tuning = Grid(resolution=5),
-    measure = mae,
-    acceleration = CPUThreads()
+@showprogress for i in 1:length(tuning_grid)
+
+    global bestcor
+    Random.seed!(ml_rng, 0)
+
+    rf_model = RandomForestRegressor(
+        n_trees = tuning_grid[i][5],
+        min_samples_leaf = tuning_grid[i][1],
+        max_depth = tuning_grid[i][2],
+        sampling_fraction = tuning_grid[i][3],
+        n_subfeatures = tuning_grid[i][4],
+        rng=ml_rng
+    )
+
+    rf_machine = machine(rf_model, X[train, :], y[train])
+    fit!(rf_machine, verbosity=0)
+
+    train_y_hat = predict(rf_machine, X[train, :]) 
+    train_mae = mean(MLJ.MLJBase.l1(train_y_hat, y[train]))
+    train_cor = Statistics.cor(train_y_hat, y[train])
+
+    test_y_hat = predict(rf_machine, X[test, :])
+    test_mae = mean(MLJ.MLJBase.l1(test_y_hat, y[test]))
+    test_cor = Statistics.cor(test_y_hat, y[test])
+
+#    machines_vector[i] = deepcopy(rf_machine)
+    train_mae_vector[i] = train_mae
+    train_cor_vector[i] = train_cor
+    test_mae_vector[i] = test_mae
+    test_cor_vector[i] = test_cor
+
+    if !isnan(test_cor)
+        if(test_cor) > bestcor
+            global bestcor = test_cor
+            global bestmodel = deepcopy(rf_machine)
+        end
+    end
+
+end
+
+println(minimum(train_mae_vector))
+println(minimum(test_mae_vector))
+println(maximum(train_cor_vector))
+sort(train_cor_vector[.!(isnan.(train_cor_vector))])
+println(maximum(test_cor_vector))
+sort(test_cor_vector[.!(isnan.(test_cor_vector))])
+
+#### Selecting model with the best performance on the test set
+
+using GLM
+
+selected_machine = bestmodel
+
+train_y_hat = predict(selected_machine, X[train, :])
+slope_correction_df = DataFrame([ train_y_hat, y[train] ], [:yhat, :y])
+slope_correction = lm(@formula(y ~ yhat), slope_correction_df)
+train_y_hat = GLM.predict(slope_correction, DataFrame(:yhat => train_y_hat))
+train_mae = mean(MLJ.MLJBase.l1(train_y_hat, y[train]))
+train_cor = Statistics.cor(train_y_hat, y[train])
+
+test_y_hat = GLM.predict(slope_correction, DataFrame(:yhat => predict(selected_machine, X[test, :])))
+test_mae = mean(MLJ.MLJBase.l1(test_y_hat, y[test]))
+test_cor = Statistics.cor(test_y_hat, y[test])
+
+#### Plotting and saving figures
+
+using CairoMakie
+
+train_set_plot = Figure(resolution = (800, 600))
+ax = Axis(train_set_plot[1, 1],
+    title = "Comparison of predicted and ground truth Current cogScore for the *TRAIN* set",
+    xlabel = "Ground Truth",
+    ylabel = "Prediction"
+)
+scatter!(ax, y[train], train_y_hat)
+ablines!(ax, 1, 1)
+annotations!( ax, ["r = $(round(train_cor; digits = 2))"], [Point(60, 100)], textsize = 40)
+
+train_set_plot
+
+save("figures/prediction_currentcogscore_trainset_comparison.png", train_set_plot)
+
+test_set_plot = Figure()
+ax = Axis(test_set_plot[1, 1],
+    title = "Comparison of predicted and ground truth Future cogScore for the *TEST* set",
+    xlabel = "Ground Truth",
+    ylabel = "Prediction"
+)
+scatter!(ax, y[test], test_y_hat)
+ablines!(ax, 1, 1)
+annotations!( ax, ["r = $(round(test_cor; digits = 2))"], [Point(60, 95)], textsize = 40)
+
+test_set_plot
+
+save("figures/prediction_currentcogscore_testset_comparison.png", test_set_plot)
+
+using DecisionTree
+
+@assert cor(
+    apply_forest(selected_machine.fitresult, Matrix(X[test, :])),
+    test_y_hat
+) == 1.0
+
+impurity_importances = impurity_importance(selected_machine.fitresult)
+impurity_ordered_idx = sortperm(impurity_importances; rev = true)
+
+impurityDf = DataFrame(
+    :Variable => names(X)[impurity_ordered_idx],
+    :Importance => impurity_importances[impurity_ordered_idx]
 )
 
-tuning_rf_machine = machine(tunable_rf_model, X_scaled, y_scaled)
+### Using linear regression to adjust future cogScore ~ futureAgeMonths and train on residuals
 
-#### Training the Machine on `train` rows of `X`
+predictionDf[sortperm(predictionDf.target), :]
+predictionDf[predictionDf.ageMonthsDelta .>= 12.0, :]
 
-fit!(tuning_rf_machine, rows=train)
+futurecogscore_futureagemonths_exploration = Figure()
+ax = Axis(futurecogscore_futureagemonths_regression[1, 1],
+    title = "futureCogScore as a function of futureAgeMonths",
+    xlabel = "futureAgeMonths",
+    ylabel = "futureCogScore"
+)
+scatter!(ax, predictionDf.futureAgeMonths, predictionDf.target)
 
-tuning_report = report(tuning_rf_machine)
-@show tuning_report.best_history_entry
-@show tuning_report.best_model
+futurecogscore_futureagemonths_exploration
 
-best_rf_model = eval(tuning_report.best_model)
+using GLM
 
-best_rf_machine = machine(best_rf_model, X_scaled, y_scaled)
+fm = @formula(target ~ futureAgeMonths)
+linear_regressor = lm(fm, predictionDf)
 
-#### Training the Machine on `train` rows of `X`
-    
-fit!(best_rf_machine, rows=train)    
+intercept = linear_regressor.model.pp.beta0[1]
+slope = linear_regressor.model.pp.beta0[2]
 
-#### Evaluating the Machine on `test` rows of `X`
+linear_prediction = GLM.predict(linearRegressor, hcat(ones(Float64, nrow(predictionDf)), predictionDf.futureAgeMonths))
+residuals = predictionDf.target .- linear_prediction
 
-train_y_hat = predict(best_rf_machine, rows=train)
+futurecogscore_futureagemonths_regression = Figure()
+ax = Axis(futurecogscore_futureagemonths_regression[1, 1],
+    title = "true futureCogScore as a function of regression output from futureAgeMonths",
+    xlabel = "futureCogScore",
+    ylabel = "estimation of futureCogScore"
+)
+scatter!(ax, predictionDf.target, linear_prediction)
 
-test_y_hat = predict(best_rf_machine, rows=test)
+futurecogscore_futureagemonths_regression
 
-train_abs_errors = MLJ.MLJBase.l1(train_y_hat, y_scaled[train]) # Absolute ErrorS
-train_mean_abs_error = mean(train_abs_errors) # Mean Absolute Error
-train_sq_errors = MLJ.MLJBase.l2(train_y_hat, y_scaled[train]) # Square ErrorS
-train_mean_sq_error = mean(train_sq_errors) # Mean Square Error
+####
 
-test_abs_errors = MLJ.MLJBase.l1(test_y_hat, y_scaled[test]) # Absolute ErrorS
-test_mean_abs_error = mean(test_abs_errors) # Mean Absolute Error
-test_sq_errors = MLJ.MLJBase.l2(test_y_hat, y_scaled[test]) # Square ErrorS
-test_mean_sq_error = mean(test_sq_errors) # Mean Square Error
-
-Statistics.cor(y_scaled[train], train_y_hat)
-Statistics.cor(y_scaled[test], test_y_hat)
-
-scatter(y_scaled[train], train_y_hat)
-scatter(y_scaled[test], test_y_hat)
+predictionDf.target .= residuals
