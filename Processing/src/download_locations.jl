277faf2c561e68d9a2cc5c7330e5217068572413
Kevin Bonham, PhD <kbonham@wellesley.edu>
2022-11-04 21:51:14 -0400

    rename setup

--- Processing/src/download_locations.jl
+++
@@ -1,6 +0,0 @@
-const taxa_osfio         = "qn4hz"
-const genefamilies_osfio = ""
-const ecs_osfio          = "hg9d5"
-const kos_osfio          = "awyts"
-const timepoints_osfio   = "pva98"
-const brain_osfio        = "yatwr"
