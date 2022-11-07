6d263dc03985964d9e0508904d8344c8665f862b
Kevin Bonham, PhD <kbonham@wellesley.edu>
2022-11-07 08:57:44 -0500

    add gene families url id

--- Setup/src/download_locations.jl
+++ Setup/src/download_locations.jl
@@ -1,7 +1,7 @@
 # Initial downloads
 
 const taxa_osfio         = "qn4hz"
-const genefamilies_osfio = ""
+const genefamilies_osfio = "75pf4"
 const ecs_osfio          = "hg9d5"
 const kos_osfio          = "awyts"
 const timepoints_osfio   = "pva98"
