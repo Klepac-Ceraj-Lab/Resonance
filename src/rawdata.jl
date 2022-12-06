4273970fee8ac7fbf09ebe9705724d36ae04d8da
Kevin Bonham, PhD <kbonham@wellesley.edu>
2022-12-06 09:34:58 -0500

    only add read_depth

--- src/rawdata.jl
+++ src/rawdata.jl
@@ -112,7 +112,7 @@ function load_raw_metaphlan()
     knead = load(ReadCounts())
     taxa = metaphlan_profiles(df.file; samples = df.sample)
     set!(taxa, df)
-    # set!(taxa, select(knead, "sample_uid"=>"sample", AsTable(["final pair1", "final pair2"])=> ByRow(row-> row[1]+row[2]) =>"read_depth"))
+    set!(taxa, select(knead, "sample_uid"=>"sample", AsTable(["final pair1", "final pair2"])=> ByRow(row-> row[1]+row[2]) =>"read_depth"))
     taxa
 end
 
