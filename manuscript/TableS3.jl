a85f42a53ed1e07fb704384fb99e5fe023fdba4e
Kevin Bonham <kevin@bonham.ch>
2023-03-24 21:48:02 -0400

    move T5 -> TS3

--- manuscript/TableS3.jl
+++ manuscript/TableS3.jl
@@ -14,16 +14,16 @@ isdir(tablefiles()) || mkpath(tablefiles())
 
 ## Building the Table
 
-table5 = DataFrame(
+tableS3 = DataFrame(
     :input_set => 1:10,
     :age_bracket => vcat( repeat([ "0 to 6 months" ], 5), repeat( [ "18 to 120 months" ], 5)),
     :microbial_feature => repeat([ "Not provided", "Taxonomic profile", "Taxonomic profile", "Functional Profile (ECs)", "Functional Profile (ECs)" ], 2),
     :demo_provided => repeat([ "yes", "no", "yes", "no", "yes" ], 2),
 )
 
-CSV.write(tablefiles("Table5.csv"), table5)
+CSV.write(joinpath("manuscript", "assets", "TableS3.csv"), tableS3)
 
-pretty_table(table5;
+pretty_table(tableS3;
     header = [ "Input set", "Age bracket", "Microbiome encoding type", "Demographics Provided? (sex, education)" ],
     backend = Val(:latex)
 )
