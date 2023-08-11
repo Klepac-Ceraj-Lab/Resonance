57e825045824a4c795b058cd86e8d03a9410e76f
Kevin Bonham <kevin@bonham.ch>
2023-08-11 11:04:40 -0400

    continue work on metabolomics

--- notes/mbx.jl
+++ notes/mbx.jl
@@ -75,22 +75,21 @@ unique(subset(mdata, "mbxsample" => ByRow(!ismissing)), "subject") |> size
 
 subset(mdata, "mbxsample" => ByRow(!ismissing)).ageMonths |> describe
 
-#-
+#- OK, you win, Claire 😭
 
-fixcol(col, n::String)                  = fixcol(col, Val{Symbol(n)}())
-fixcol(col, n::Val)                     = coalesce.(col, 0)
-fixcol(col, n::Val{:MZ})                = convert(Vector{Float64}, col)
-fixcol(col, n::Val{:RT})                = convert(Vector{Float64}, col)
-fixcol(col, n::Val{:Method})            = convert(Vector{String}, col)
-fixcol(col, n::Val{:Compound_ID})       = convert(Vector{String}, col)
-fixcol(col, n::Val{:HMDB_ID})           = convert(Vector{Union{Missing,String}}, col)
-fixcol(col, n::Val{:Metabolite})        = convert(Vector{Union{Missing,String}}, col)
-fixcol(col, n::Val{:HMDB_ID_Certainty}) = map(col) do c
-    c isa Int && return c
-    ismissing(c) && return c
-    c == "NA" && return 0
+function fixcol(col, name)
+    name == "MZ"                ? convert(Vector{Float64}, col) :
+    name == "RT"                ? convert(Vector{Float64}, col) :
+    name == "Method"            ? fill(first(col) == "HIL-pos" ? "HILIC-pos" : first(col), length(col)) :
+    name == "Compound_ID"       ? convert(Vector{String}, col) :
+    name == "HMDB_ID"           ? convert(Vector{Union{Missing,String}}, col) :
+    name == "Metabolite"        ? convert(Vector{Union{Missing,String}}, col) :
+    name == "HMDB_ID_Certainty" ? map(col) do c
+        c isa Int && return c
+        ismissing(c) && return c
+        c == "NA" && return 0
+    end : coalesce.(col, 0)
 end
-
 #-
 
 mbx_xlsx_files = filter(f-> endswith(f, ".xlsx") && !contains(f, r"centric"i), readdir("input/", join=true))
@@ -129,39 +128,6 @@ mbx_long = mapreduce(vcat, mbx_xlsx_files) do file
     end) => "sample")
 end
 
-append!(mbx_long, let file = mbx_xlsx_files[5]
-    xf = XLSX.readxlsx(file)
-    sh = xf[first(XLSX.sheetnames(xf))]
-    mat = sh[:]
-    startrow = findfirst(!ismissing, mat[:,1])
-    endcol = let i = findfirst(ismissing, mat[startrow, :])
-        isnothing(i) ? size(mat, 2) : i - 1
-    end
-
-    df = DataFrame(mat[startrow+1:size(mat, 1), 1:endcol], vec(mat[startrow, 1:endcol]); makeunique=true)
-    rename!(df, first(names(df, r"HMDB_ID_")) => "HMDB_ID_Certainty")
-    for n in names(df)
-        df[!, n] = fixcol(df[!, n], n)
-    end
-
-    df = subset(stack(df, Not(["MZ", "RT", "Method", "Compound_ID", "HMDB_ID", "Metabolite", "HMDB_ID_Certainty"]); 
-                variable_name = "sample"),
-            "sample" => ByRow(s-> !startswith(s, "PRE"))
-    )
-    transform!(df, "sample"=> ByRow(mbx-> begin
-        startswith(mbx, "FE") && return mbx
-        if startswith(mbx, "SD")
-            m = match(r"SD(\d+)([A-Z])", mbx)
-            isnothing(m) && error("SD sample $mbx doesn't match pattern")
-            sid = lpad(m[1], 4, "0")
-            tp = findfirst(c-> "$c" == m[2], "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-            mbx = string("C", sid, "_", tp, "E", "_1A")
-        end
-        mbxid = get(biospdict, mbx, nothing)
-        isnothing(mbxid) && return mbx
-        return mbxid
-    end) => "sample")
-end)
 
 @chain mbx_long begin
     groupby(["Method", "sample"])
@@ -181,3 +147,63 @@ metabolites[findall(m-> contains(m, r"quino"i), metabolites)]
 using Arrow
 
 Arrow.write("input/metabolites.arrow", mbx_long)
+
+#-
+
+using Resonance
+using VKCComputing
+using XLSX
+using ThreadsX
+using Chain
+using Arrow
+
+VKCComputing.set_readonly_pat!()
+
+base = LocalBase()
+
+mbx_long = Arrow.Table("input/metabolites.arrow") |> DataFrame
+# mbx_long = subset(mbx_long, "sample"=> ByRow(s-> !startswith(s, "M")))
+
+transform!(groupby(mbx_long, "sample"), "sample"=> (scol-> begin
+    len = length(scol)
+    sample = first(scol)
+
+    # https://vkc-lab.slack.com/archives/C6A0Z4Z8X/p1691697373467609
+    if sample == "C1227_3E_1A"
+        sample = "FE01922"
+    elseif sample == "C1162_3E_1A"
+        sample = "FE01852"
+    elseif sample == "FE2790"
+        sample = "FE02790"
+    elseif sample == "M0932_7E_1A"
+        sample = "FE01759"
+    elseif sample == "C11680_4E_1A"
+        sample = "FE01935"
+    end
+
+    default_ret = (; subject = fill(missing, len), timepoint = fill(missing, len), biospecimen = fill(missing, len))
+    biosp = get(base["Biospecimens"], sample, nothing)
+    if isnothing(biosp)
+        alias = get(base["Aliases"], sample, nothing)
+        (isnothing(alias) || !haskey(alias, :biospecimens)) && return default_ret
+
+        biosp = base[first(alias[:biospecimens])]
+    end
+
+    timepoint = get(biosp, :collection, missing)
+    subject = get(get(base, first(get(biosp, :subject, ("",))), Dict()), :uid, missing)
+    subject = ismissing(subject) ? missing : parse(Int, replace(subject, "resonance-"=>""))
+    biospecimen = biosp[:uid]
+    
+    return (; subject = fill(subject, len), timepoint = fill(timepoint, len), biospecimen = fill(biospecimen, len))
+    
+end) => ["subject", "timepoint", "biospecimen"])
+
+#-
+
+newmeta = CSV.read("input/ECHO_metabolomics_preselectedinputs.csv", DataFrame)
+subtp = Set([(row.subject,row.timepoint) for row in eachrow(newmeta)])
+mbx_long = subset(mbx_long, AsTable(["subject", "timepoint"])=> ByRow(nt-> (nt.subject, nt.timepoint) ∈ subtp))
+
+#-
+
