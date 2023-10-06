6a6c4fd77ecdc00a62709163758b086438145505
Kevin Bonham, PhD <kbonham@wellesley.edu>
2023-10-06 14:08:27 -0400

    include all samples

--- manuscript/Table2.jl
+++ manuscript/Table2.jl
@@ -7,11 +7,11 @@ using DataFrames.PrettyTables
 isdir(tablefiles()) || mkpath(tablefiles())
 
 fsea = let
-    fsea_all = CSV.read(scratchfiles("figure2", "fsea_all_00to120.csv"), DataFrame)
+    fsea_all = CSV.read(tablefiles("figure2", "fsea_all_00to120.csv"), DataFrame)
     fsea_all.group .= "00to120"
-    fsea_u6 = CSV.read(scratchfiles("figure2", "fsea_all_00to06.csv"), DataFrame)
+    fsea_u6 = CSV.read(tablefiles("figure2", "fsea_all_00to06.csv"), DataFrame)
     fsea_u6.group .= "00to06"
-    fsea_o18 = CSV.read(scratchfiles("figure2", "fsea_all_18to120.csv"), DataFrame)
+    fsea_o18 = CSV.read(tablefiles("figure2", "fsea_all_18to120.csv"), DataFrame)
     fsea_o18.group .= "18to120"
     vcat(fsea_all, fsea_u6, fsea_o18)
 end
@@ -40,16 +40,18 @@ end
 
 fseaout = DataFrame(geneset=unique(fsea.geneset))
 for g in levels(fsea.group)
-    df = select(subset(fsea, "group"=> ByRow(==(g))), "enrichment", "qvalue")
+    df = select(leftjoin(select(fseaout, "geneset"), subset(fsea, "group"=> ByRow(==(g))); on="geneset"), "enrichment", "qvalue")
     rename!(df, ["$(g)_enrichment", "$(g)_qvalue"])
     fseaout = hcat(fseaout, df)
 end
-
+for col in names(fseaout, Not("geneset"))
+    fseaout[!, col] = coalesce.(fseaout[!, col], NaN)
+end
 
 CSV.write(tablefiles("Table2.csv"), fseaout)
 formatter = (v, i, j) -> v isa AbstractFloat ? round(v, digits = 3) : v
-h1 = LatexHighlighter((data, i, j) -> j in (3,5,7) && data[i,j] < 0.2 && data[i,j-1] < 0, ["color{red}","textbf"])
-h2 = LatexHighlighter((data, i, j) -> j in (3,5,7) && data[i,j] < 0.2 && data[i,j-1] > 0, ["color{blue}","textbf"])
+h1 = LatexHighlighter((data, i, j) -> !ismissing(j) && j in (3,5,7) && data[i,j] < 0.2 && data[i,j-1] < 0, ["color{blue}","textbf"])
+h2 = LatexHighlighter((data, i, j) -> !ismissing(j) && j in (3,5,7) && data[i,j] < 0.2 && data[i,j-1] > 0, ["color{red}","textbf"])
 
 pretty_table(fseaout; highlighters=(h1,h2), formatters=ft_round(3),
     header = (map(n-> replace(replace(n, r"_.+" => ""), "geneset"=>"Age group"), names(fseaout)),
