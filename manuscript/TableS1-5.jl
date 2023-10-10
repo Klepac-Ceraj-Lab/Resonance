3e5c73445e8edaf04afd97e97bcd939cef1e2025
Guilherme Fabur Bottino <gfahurbottino@gmail.com>
2023-10-10 14:11:00 -0400

    moved Suppl table generation to unified code

--- manuscript/TableS1-5.jl
+++ manuscript/TableS1-5.jl
@@ -10,10 +10,28 @@ using MLJ
 using DataFrames.PrettyTables
 
 isdir(tablefiles()) || mkpath(tablefiles())
-
 RandomForestRegressor = MLJ.@load RandomForestRegressor pkg=DecisionTree
 
 ## 1. Loading the model files
+
+### 1.1. Concurrent composite scores
+regression_currentCogScores_00to06mo_onlydemo = JLD2.load(modelfiles("regression_currentCogScores_00to06mo_onlydemo.jld"))["regression_currentCogScores_00to06mo_onlydemo"]
+regression_currentCogScores_00to06mo_onlytaxa = JLD2.load(modelfiles("regression_currentCogScores_00to06mo_onlytaxa.jld"))["regression_currentCogScores_00to06mo_onlytaxa"]
+regression_currentCogScores_00to06mo_demoplustaxa = JLD2.load(modelfiles("regression_currentCogScores_00to06mo_demoplustaxa.jld"))["regression_currentCogScores_00to06mo_demoplustaxa"]
+regression_currentCogScores_00to06mo_onlyecs = JLD2.load(modelfiles("regression_currentCogScores_00to06mo_onlyecs.jld"))["regression_currentCogScores_00to06mo_onlyecs"]
+regression_currentCogScores_00to06mo_demoplusecs = JLD2.load(modelfiles("regression_currentCogScores_00to06mo_demoplusecs.jld"))["regression_currentCogScores_00to06mo_demoplusecs"]
+regression_currentCogScores_18to120mo_onlydemo = JLD2.load(modelfiles("regression_currentCogScores_18to120mo_onlydemo.jld"))["regression_currentCogScores_18to120mo_onlydemo"]
+regression_currentCogScores_18to120mo_onlytaxa = JLD2.load(modelfiles("regression_currentCogScores_18to120mo_onlytaxa.jld"))["regression_currentCogScores_18to120mo_onlytaxa"]
+regression_currentCogScores_18to120mo_demoplustaxa = JLD2.load(modelfiles("regression_currentCogScores_18to120mo_demoplustaxa.jld"))["regression_currentCogScores_18to120mo_demoplustaxa"]
+regression_currentCogScores_18to120mo_onlyecs = JLD2.load(modelfiles("regression_currentCogScores_18to120mo_onlyecs.jld"))["regression_currentCogScores_18to120mo_onlyecs"]
+regression_currentCogScores_18to120mo_demoplusecs = JLD2.load(modelfiles("regression_currentCogScores_18to120mo_demoplusecs.jld"))["regression_currentCogScores_18to120mo_demoplusecs"]
+regression_currentCogScores_00to120mo_onlydemo = JLD2.load(modelfiles("regression_currentCogScores_00to120mo_onlydemo.jld"))["regression_currentCogScores_00to120mo_onlydemo"]
+regression_currentCogScores_00to120mo_onlytaxa = JLD2.load(modelfiles("regression_currentCogScores_00to120mo_onlytaxa.jld"))["regression_currentCogScores_00to120mo_onlytaxa"]
+regression_currentCogScores_00to120mo_demoplustaxa = JLD2.load(modelfiles("regression_currentCogScores_00to120mo_demoplustaxa.jld"))["regression_currentCogScores_00to120mo_demoplustaxa"]
+regression_currentCogScores_00to120mo_onlyecs = JLD2.load(modelfiles("regression_currentCogScores_00to120mo_onlyecs.jld"))["regression_currentCogScores_00to120mo_onlyecs"]
+regression_currentCogScores_00to120mo_demoplusecs = JLD2.load(modelfiles("regression_currentCogScores_00to120mo_demoplusecs.jld"))["regression_currentCogScores_00to120mo_demoplusecs"]
+
+### 1.2. Concurrent Expressive Language scores
 regression_currentExpressiveLanguages_00to06mo_onlydemo = JLD2.load(modelfiles("regression_currentExpressiveLanguages_00to06mo_onlydemo.jld"))["regression_currentExpressiveLanguages_00to06mo_onlydemo"]
 regression_currentExpressiveLanguages_00to06mo_onlytaxa = JLD2.load(modelfiles("regression_currentExpressiveLanguages_00to06mo_onlytaxa.jld"))["regression_currentExpressiveLanguages_00to06mo_onlytaxa"]
 regression_currentExpressiveLanguages_00to06mo_demoplustaxa = JLD2.load(modelfiles("regression_currentExpressiveLanguages_00to06mo_demoplustaxa.jld"))["regression_currentExpressiveLanguages_00to06mo_demoplustaxa"]
@@ -29,6 +47,8 @@ regression_currentExpressiveLanguages_00to120mo_onlytaxa = JLD2.load(modelfiles(
 regression_currentExpressiveLanguages_00to120mo_demoplustaxa = JLD2.load(modelfiles("regression_currentExpressiveLanguages_00to120mo_demoplustaxa.jld"))["regression_currentExpressiveLanguages_00to120mo_demoplustaxa"]
 regression_currentExpressiveLanguages_00to120mo_onlyecs = JLD2.load(modelfiles("regression_currentExpressiveLanguages_00to120mo_onlyecs.jld"))["regression_currentExpressiveLanguages_00to120mo_onlyecs"]
 regression_currentExpressiveLanguages_00to120mo_demoplusecs = JLD2.load(modelfiles("regression_currentExpressiveLanguages_00to120mo_demoplusecs.jld"))["regression_currentExpressiveLanguages_00to120mo_demoplusecs"]
+
+### 1.2. Concurrent Gross Motor scores
 regression_currentGrossMotors_00to06mo_onlydemo = JLD2.load(modelfiles("regression_currentGrossMotors_00to06mo_onlydemo.jld"))["regression_currentGrossMotors_00to06mo_onlydemo"]
 regression_currentGrossMotors_00to06mo_onlytaxa = JLD2.load(modelfiles("regression_currentGrossMotors_00to06mo_onlytaxa.jld"))["regression_currentGrossMotors_00to06mo_onlytaxa"]
 regression_currentGrossMotors_00to06mo_demoplustaxa = JLD2.load(modelfiles("regression_currentGrossMotors_00to06mo_demoplustaxa.jld"))["regression_currentGrossMotors_00to06mo_demoplustaxa"]
@@ -44,6 +64,8 @@ regression_currentGrossMotors_00to120mo_onlytaxa = JLD2.load(modelfiles("regress
 regression_currentGrossMotors_00to120mo_demoplustaxa = JLD2.load(modelfiles("regression_currentGrossMotors_00to120mo_demoplustaxa.jld"))["regression_currentGrossMotors_00to120mo_demoplustaxa"]
 regression_currentGrossMotors_00to120mo_onlyecs = JLD2.load(modelfiles("regression_currentGrossMotors_00to120mo_onlyecs.jld"))["regression_currentGrossMotors_00to120mo_onlyecs"]
 regression_currentGrossMotors_00to120mo_demoplusecs = JLD2.load(modelfiles("regression_currentGrossMotors_00to120mo_demoplusecs.jld"))["regression_currentGrossMotors_00to120mo_demoplusecs"]
+
+### 1.2. Concurrent Visual Reception scores
 regression_currentVisualReceptions_00to06mo_onlydemo = JLD2.load(modelfiles("regression_currentVisualReceptions_00to06mo_onlydemo.jld"))["regression_currentVisualReceptions_00to06mo_onlydemo"]
 regression_currentVisualReceptions_00to06mo_onlytaxa = JLD2.load(modelfiles("regression_currentVisualReceptions_00to06mo_onlytaxa.jld"))["regression_currentVisualReceptions_00to06mo_onlytaxa"]
 regression_currentVisualReceptions_00to06mo_demoplustaxa = JLD2.load(modelfiles("regression_currentVisualReceptions_00to06mo_demoplustaxa.jld"))["regression_currentVisualReceptions_00to06mo_demoplustaxa"]
@@ -59,70 +81,22 @@ regression_currentVisualReceptions_00to120mo_onlytaxa = JLD2.load(modelfiles("re
 regression_currentVisualReceptions_00to120mo_demoplustaxa = JLD2.load(modelfiles("regression_currentVisualReceptions_00to120mo_demoplustaxa.jld"))["regression_currentVisualReceptions_00to120mo_demoplustaxa"]
 regression_currentVisualReceptions_00to120mo_onlyecs = JLD2.load(modelfiles("regression_currentVisualReceptions_00to120mo_onlyecs.jld"))["regression_currentVisualReceptions_00to120mo_onlyecs"]
 regression_currentVisualReceptions_00to120mo_demoplusecs = JLD2.load(modelfiles("regression_currentVisualReceptions_00to120mo_demoplusecs.jld"))["regression_currentVisualReceptions_00to120mo_demoplusecs"]
-## 2. Defining Suppl Table Functions
-function singlemodel_importances_suppltable(rf_model)
-    rf_model_importances = weighted_hpimportances(rf_model; normalize_importances=false, change_hashnames=false)
-    rf_model_importances.relativeWeightedImportance = rf_model_importances.weightedImportance ./ sum(skipmissing(rf_model_importances.weightedImportance))
-    rf_model_importances.cumulativeWeightedImportance = cumsum(rf_model_importances.relativeWeightedImportance)
-    return rf_model_importances
-end
 
-function prettyformat_suppltable!(suptbl)
-    suptbl = @chain suptbl begin
-        insertcols!(1, :rank => 1:nrow(suptbl))
-        transform!(:weightedImportance => (x -> string.(round.(x; digits = 5))) => :weightedImportance, renamecols = false)
-        transform!(:relativeWeightedImportance => (x -> string.(round.(100*x; digits = 2)).*" %"), renamecols = false)
-        transform!(:cumulativeWeightedImportance => (x -> string.(round.(100*x; digits = 2)).*" %"), renamecols = false)
-    end    
-    return suptbl
-end
+### 1.2. Future composite scores
+regression_futureCogScores_onlydemo = JLD2.load(modelfiles("regression_futureCogScores_onlydemo.jld"))["regression_futureCogScores_onlydemo"]
+regression_futureCogScores_onlytaxa = JLD2.load(modelfiles("regression_futureCogScores_onlytaxa.jld"))["regression_futureCogScores_onlytaxa"]
+regression_futureCogScores_demoplustaxa = JLD2.load(modelfiles("regression_futureCogScores_demoplustaxa.jld"))["regression_futureCogScores_demoplustaxa"]
+regression_futureCogScores_onlyecs = JLD2.load(modelfiles("regression_futureCogScores_onlyecs.jld"))["regression_futureCogScores_onlyecs"]
+regression_futureCogScores_demoplusecs = JLD2.load(modelfiles("regression_futureCogScores_demoplusecs.jld"))["regression_futureCogScores_demoplusecs"]
+
+## 2. Defining Suppl Table Functions
 
 models_to_report = [
-    regression_currentExpressiveLanguages_00to06mo_onlydemo,
-    regression_currentExpressiveLanguages_00to06mo_onlytaxa,
-    regression_currentExpressiveLanguages_00to06mo_demoplustaxa,
-    regression_currentExpressiveLanguages_00to06mo_onlyecs,
-    regression_currentExpressiveLanguages_00to06mo_demoplusecs,
-    regression_currentExpressiveLanguages_18to120mo_onlydemo,
+    regression_currentCogScores_18to120mo_onlytaxa,
     regression_currentExpressiveLanguages_18to120mo_onlytaxa,
-    regression_currentExpressiveLanguages_18to120mo_demoplustaxa,
-    regression_currentExpressiveLanguages_18to120mo_onlyecs,
-    regression_currentExpressiveLanguages_18to120mo_demoplusecs,
-    regression_currentExpressiveLanguages_00to120mo_onlydemo,
-    regression_currentExpressiveLanguages_00to120mo_onlytaxa,
-    regression_currentExpressiveLanguages_00to120mo_demoplustaxa,
-    regression_currentExpressiveLanguages_00to120mo_onlyecs,
-    regression_currentExpressiveLanguages_00to120mo_demoplusecs,
-    regression_currentGrossMotors_00to06mo_onlydemo,
-    regression_currentGrossMotors_00to06mo_onlytaxa,
-    regression_currentGrossMotors_00to06mo_demoplustaxa,
-    regression_currentGrossMotors_00to06mo_onlyecs,
-    regression_currentGrossMotors_00to06mo_demoplusecs,
-    regression_currentGrossMotors_18to120mo_onlydemo,
     regression_currentGrossMotors_18to120mo_onlytaxa,
-    regression_currentGrossMotors_18to120mo_demoplustaxa,
-    regression_currentGrossMotors_18to120mo_onlyecs,
-    regression_currentGrossMotors_18to120mo_demoplusecs,
-    regression_currentGrossMotors_00to120mo_onlydemo,
-    regression_currentGrossMotors_00to120mo_onlytaxa,
-    regression_currentGrossMotors_00to120mo_demoplustaxa,
-    regression_currentGrossMotors_00to120mo_onlyecs,
-    regression_currentGrossMotors_00to120mo_demoplusecs,    
-    regression_currentVisualReceptions_00to06mo_onlydemo,
-    regression_currentVisualReceptions_00to06mo_onlytaxa,
-    regression_currentVisualReceptions_00to06mo_demoplustaxa,
-    regression_currentVisualReceptions_00to06mo_onlyecs,
-    regression_currentVisualReceptions_00to06mo_demoplusecs,
-    regression_currentVisualReceptions_18to120mo_onlydemo,
     regression_currentVisualReceptions_18to120mo_onlytaxa,
-    regression_currentVisualReceptions_18to120mo_demoplustaxa,
-    regression_currentVisualReceptions_18to120mo_onlyecs,
-    regression_currentVisualReceptions_18to120mo_demoplusecs,
-    regression_currentVisualReceptions_00to120mo_onlydemo,
-    regression_currentVisualReceptions_00to120mo_onlytaxa,
-    regression_currentVisualReceptions_00to120mo_demoplustaxa,
-    regression_currentVisualReceptions_00to120mo_onlyecs,
-    regression_currentVisualReceptions_00to120mo_demoplusecs,    
+    regression_futureCogScores_onlytaxa
 ]
 
 ## 4. LaTeX outputs
